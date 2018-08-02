ace8e6d01652409b8ea3e9fcbefe9eebfb3917a1
Florian R. Hölzlwimmer <git.ich@frhoelzlwimmer.de>
2018-08-02 13:34:58 +0200

    fix missing imports

--- diffxpy/unit_test/test_single.py
+++ diffxpy/unit_test/test_single.py
@@ -1,7 +1,10 @@
 import unittest
 
 import numpy as np
+import pandas as pd
 import scipy.stats as stats
+
+from batchglm.api.models.nb_glm import Simulator, Estimator, InputData
 import diffxpy as de
 
 
@@ -21,25 +24,25 @@ class TestSingle(unittest.TestCase):
         sim = Simulator(num_observations=n_cells, num_features=n_genes)
         sim.generate_sample_description(num_batches=0, num_confounders=0)
         sim.generate()
-
+        
         random_sample_description = pd.DataFrame({
             "condition": np.random.randint(2, size=sim.num_observations)
         })
-                
+        
         test = de.test_wald_loc(
-            data = sim.X, 
+            data=sim.X,
             factor_loc_totest="condition",
-            formula="~ 1 + condition", 
+            formula="~ 1 + condition",
             sample_description=random_sample_description,
             training_strategy='QUICK'
         )
-         
+        
         # Compare p-value distribution under null model against uniform distribution.
         pval_h0 = stats.kstest(test.pval, 'uniform').pvalue
         
         print('KS-test pvalue for null model match of test_wald_loc(): %f' % pval_h0)
         return pval_h0
-
+    
     def test_lrt(self, n_cells: int = 1000, n_genes: int = 1000):
         """
         Test if de.test_lrt() generates a uniform p-value distribution
@@ -54,25 +57,25 @@ class TestSingle(unittest.TestCase):
         sim = Simulator(num_observations=n_cells, num_features=n_genes)
         sim.generate_sample_description(num_batches=0, num_confounders=0)
         sim.generate()
-
+        
         random_sample_description = pd.DataFrame({
             "condition": np.random.randint(2, size=sim.num_observations)
         })
-                
+        
         test = de.test_lrt(
-            data = sim.X, 
-            full_formula="~ 1 + condition", 
-            reduced_formula="~ 1", 
+            data=sim.X,
+            full_formula="~ 1 + condition",
+            reduced_formula="~ 1",
             sample_description=random_sample_description,
             training_strategy='QUICK'
         )
-         
+        
         # Compare p-value distribution under null model against uniform distribution.
         pval_h0 = stats.kstest(test.pval, 'uniform').pvalue
         
         print('KS-test pvalue for null model match of test_lrt(): %f' % pval_h0)
         return pval_h0
-
+    
     def test_t_test(self, n_cells: int = 1000, n_genes: int = 1000):
         """
         Test if de.test_t_test() generates a uniform p-value distribution
@@ -87,23 +90,23 @@ class TestSingle(unittest.TestCase):
         sim = Simulator(num_observations=n_cells, num_features=n_genes)
         sim.generate_sample_description(num_batches=0, num_confounders=0)
         sim.generate()
-
+        
         random_sample_description = pd.DataFrame({
             "condition": np.random.randint(2, size=sim.num_observations)
         })
-                
+        
         test = de.test_t_test(
-            data = sim.X, 
+            data=sim.X,
             grouping="condition",
             sample_description=random_sample_description
         )
-         
+        
         # Compare p-value distribution under null model against uniform distribution.
         pval_h0 = stats.kstest(test.pval, 'uniform').pvalue
         
         print('KS-test pvalue for null model match of test_t_test(): %f' % pval_h0)
         return pval_h0
-
+    
     def test_wilcoxon(self, n_cells: int = 1000, n_genes: int = 1000):
         """
         Test if de.test_wilcoxon() generates a uniform p-value distribution
@@ -118,17 +121,17 @@ class TestSingle(unittest.TestCase):
         sim = Simulator(num_observations=n_cells, num_features=n_genes)
         sim.generate_sample_description(num_batches=0, num_confounders=0)
         sim.generate()
-
+        
         random_sample_description = pd.DataFrame({
             "condition": np.random.randint(2, size=sim.num_observations)
         })
-                
+        
         test = de.test_wilcoxon(
-            data = sim.X, 
+            data=sim.X,
             grouping="condition",
             sample_description=random_sample_description
         )
-         
+        
         # Compare p-value distribution under null model against uniform distribution.
         pval_h0 = stats.kstest(test.pval, 'uniform').pvalue
         
