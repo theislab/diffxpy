44eff1967b30be74f904aae69ca37404c33ba811
Florian R. Hölzlwimmer <git.ich@frhoelzlwimmer.de>
2018-08-02 16:14:16 +0200

    add sparse anndata unit test

--- diffxpy/unit_test/test_single.py
+++ diffxpy/unit_test/test_single.py
@@ -3,6 +3,8 @@ import unittest
 import numpy as np
 import pandas as pd
 import scipy.stats as stats
+import scipy.sparse
+import anndata
 
 from batchglm.api.models.nb_glm import Simulator, Estimator, InputData
 import diffxpy as de
@@ -31,6 +33,39 @@ class TestSingle(unittest.TestCase):
         
         test = de.test_wald_loc(
             data=sim.X,
+            factor_loc_totest="condition",
+            formula="~ 1 + condition",
+            sample_description=random_sample_description,
+            training_strategy='QUICK'
+        )
+        
+        # Compare p-value distribution under null model against uniform distribution.
+        pval_h0 = stats.kstest(test.pval, 'uniform').pvalue
+        
+        print('KS-test pvalue for null model match of test_wald_loc(): %f' % pval_h0)
+        return pval_h0
+
+    def test_sparse_anndata(self, n_cells: int = 1000, n_genes: int = 1000):
+        """
+        Test if de.test_wald_loc() generates a uniform p-value distribution
+        if it is given data simulated based on the null model. Returns the p-value
+        of the two-side Kolmgorov-Smirnov test for equality of the observed
+        p-value distriubution and a uniform distribution.
+
+        :param n_cells: Number of cells to simulate (number of observations per test).
+        :param n_genes: Number of genes to simulate (number of tests).
+        """
+        
+        sim = Simulator(num_observations=n_cells, num_features=n_genes)
+        sim.generate_sample_description(num_batches=0, num_confounders=0)
+        sim.generate()
+        
+        random_sample_description = pd.DataFrame({
+            "condition": np.random.randint(2, size=sim.num_observations)
+        })
+        
+        test = de.test_wald_loc(
+            data=anndata.AnnData(scipy.sparse.csr_matrix(sim.X.values)),
             factor_loc_totest="condition",
             formula="~ 1 + condition",
             sample_description=random_sample_description,
