e05095d7a3b79ee28042bd3501f0b92b3767de9f
david.seb.fischer <david.seb.fischer@gmail.com>
2018-08-02 11:58:37 +0200

    exposed alpha to user in correction.correct() for completeness, dont need this in our DE tests.

--- diffxpy/correction.py
+++ diffxpy/correction.py
@@ -1,6 +1,6 @@
 import statsmodels.stats.multitest
 
-def correct(pvals, method="fdr_bh"):
+def correct(pvals, method="fdr_bh", alpha=0.05):
     """
     Performs multiple testing corrections available in statsmodels.stats.multitest.multipletests().
     
@@ -9,5 +9,5 @@ def correct(pvals, method="fdr_bh"):
             Browse available methods in the annotation of statsmodels.stats.multitest.multipletests().
     """
     qval = statsmodels.stats.multitest.multipletests(pvals=pvals, 
-    	alpha=0.05, method=method, is_sorted=False, returnsorted=False)[1]
+    	alpha=alpha, method=method, is_sorted=False, returnsorted=False)[1]
     return qval
