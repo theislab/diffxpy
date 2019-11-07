e24d3033b6feaf5b1588dc937651a1bd3ce0ac22
david.seb.fischer <david.seb.fischer@gmail.com>
2019-11-07 08:43:44 +0100

    fixed bugs in DET continuous integration with dask

--- diffxpy/unit_test/test_continuous_null.py
+++ diffxpy/unit_test/test_continuous_null.py
@@ -5,7 +5,7 @@ import pandas as pd
 import scipy.stats as stats
 import logging
 
-from batchglm.api.models.tf1.glm_nb import Simulator
+from batchglm.api.models.numpy.glm_nb import Simulator
 import diffxpy.api as de
 
 
